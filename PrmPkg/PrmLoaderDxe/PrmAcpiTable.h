06e203d057ee5b4b19b33e219f3c54ae6a5ad668
Michael Kubacki <michael.kubacki@microsoft.com>
2020-04-07 11:26:52 -0700

    PrmPkg/PrmLoaderDxe: Add initial driver
    This driver serves as a generic PRM module loader driver. It is a key
    driver in the PRM firmware infrastructure. Its responsibilities are split
    into three phases:
    
      1. Discovery - Discover all valid PRM modules present in the loaded
         firmware environment.
      2. Processing - For each PRM module discovered, extract relevant
         information to publish the PRM ACPI table.
      3. Publishing - Publish the PRM ACPI table (PRMT) using the
         information gathered in the processing phase.
    
    As of this commit, this is an initial implementation of the driver.
    Initial meaning it has not been extensively refactored for better
    organization or optimization.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

---
+++ PrmPkg/PrmLoaderDxe/PrmAcpiTable.h
@@ -0,0 +1,95 @@
+/** @file
+
+  Definition for the Platform Runtime Mechanism (PRM) ACPI table (PRMT).
+
+  Copyright (c) Microsoft Corporation
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef _PRMT_ACPI_TABLE_H_
+#define _PRMT_ACPI_TABLE_H_
+
+#include <Base.h>
+#include <IndustryStandard/Acpi10.h>
+
+#define PRM_TABLE_SIGNATURE         SIGNATURE_32 ('P', 'R', 'M', 'T')
+#define PRM_TABLE_REVISION          0x0
+#define PRM_TABLE_OEM_ID            "MSFT  "
+#define PRM_TABLE_OEM_TABLE_ID      SIGNATURE_64 ('T', 'E', 'S', 'T', 'P', 'R', 'M', 'T')
+#define PRM_TABLE_OEM_REVISION      0x00000001
+#define PRM_TABLE_CREATOR_ID        SIGNATURE_32 ('M', 'S', 'F', 'T')
+#define PRM_TABLE_CREATOR_REVISION  0x00010001
+
+#define PRM_MODULE_INFORMATION_STRUCT_REVISION    0x00
+#define PRM_HANDLER_INFORMATION_STRUCT_REVISION   0x00
+
+#pragma pack(push, 1)
+
+//
+// Platform Runtime Mechanism (PRM) ACPI Table (PRMT) structures
+//
+typedef struct {
+  UINT16                              StructureRevision;          ///< Revision of this structure
+  UINT16                              StructureLength;            ///< Length in bytes of this structure
+  GUID                                HandlerGuid;                ///< GUID of the PRM handler for this structure
+  UINT32                              Reserved;                   ///< Reserved for future use
+  UINT64                              HandlerPhysicalAddress;     ///< Physical address of this PRM handler
+  UINT64                              PrmContextBuffer;           ///< Physical address of the context buffer for this
+                                                                  ///< PRM handler (PRM_CONTEXT_BUFFER *)
+} PRM_HANDLER_INFORMATION_STRUCT;
+
+typedef struct {
+  UINT16                              StructureRevision;          ///< Revision of this structure
+  UINT16                              StructureLength;            ///< Length in bytes of this structure including the
+                                                                  ///< variable length PRM Handler Info array
+  GUID                                ModuleGuid;                 ///< GUID of the PRM module for this structure
+  UINT16                              ModuleMajorRevision;        ///< PRM module major revision
+  UINT16                              ModuleMinorRevision;        ///< PRM module minor revision
+  UINT32                              NumberPrmHandlerInfo;       ///< Number of entries in the Handler Info array
+  UINT32                              OffsetPrmHandlerInfo;       ///< Offset in bytes from the beginning of this
+                                                                  ///< structure to the Handler Info array
+  UINT64                              ModuleUpdateLock;           ///< Physical address of the PRM Module Update Lock
+                                                                  ///< descriptor (PRM_MODULE_UPDATE_LOCK_DESCRIPTOR *)
+  UINT64                              RuntimeMmioRanges;          ///< Physical address of the PRM MMIO Ranges
+                                                                  ///< structure (PRM_MODULE_RUNTIME_MMIO_RANGES *)
+  PRM_HANDLER_INFORMATION_STRUCT      PrmHandlerInfoStructure[1];
+} PRM_MODULE_INFORMATION_STRUCT;
+
+typedef struct {
+  EFI_ACPI_DESCRIPTION_HEADER         Header;                     ///< Standard ACPI description header
+  UINT32                              OffsetPrmModuleInfo;        ///< Offset in bytes from the beginning of this
+                                                                  ///< structure to the PRM Module Info array
+  UINT32                              NumberPrmModuleInfo;        ///< Number of entries in the PRM Module Info array
+  PRM_MODULE_INFORMATION_STRUCT       PrmModuleInfoStructure[1];
+} PRM_ACPI_DESCRIPTION_TABLE;
+
+#pragma pack(pop)
+
+//
+// Helper macros to build PRM Information structures
+//
+// Todo: Revisit whether to use; currently both macros are not used
+//
+#define PRM_MODULE_INFORMATION_STRUCTURE(ModuleGuid, ModuleRevision, NumberPrmHandlerInfo, PrmHanderInfoStructureArray) {               \
+    {                                                                                                                                        \
+      PRM_MODULE_INFORMATION_STRUCT_REVISION,                              /* UINT16    StructureRevision;   */                         \
+      (OFFSET_OF (PRM_MODULE_INFORMATION_STRUCT, PrmHandlerInfoStructure) + (NumberPrmHandlerInfo * sizeof (PRM_HANDLER_INFORMATION_STRUCT)))   /* UINT16    StructureLength;     */ \
+      ModuleGuid,                                                          /* GUID      ModuleGuid;          */                         \
+      ModuleRevision,                                                      /* UINT16    ModuleRevision       */                         \
+      NumberPrmHandlerInfo,                                                /* UINT16    NumberPrmHandlerInfo */                         \
+      OFFSET_OF (PRM_MODULE_INFORMATION_STRUCT, OffsetPrmHandlerInfo),     /* UINT32    OffsetPrmHandlerInfo */                         \
+      PrmHanderInfoStructureArray                                          /* PRM_HANDLER_INFORMATION_STRUCT PrmHandlerInfoStructure */ \
+    } \
+  }
+
+#define PRM_HANDLER_INFORMATION_STRUCTURE(HandlerGuid, HandlerPhysicalAddress) {                                                   \
+    {                                                                                                                              \
+      PRM_HANDLER_INFORMATION_STRUCT_REVISION,                             /* UINT16                  StructureRevision;      */   \
+      sizeof (PRM_HANDLER_INFORMATION_STRUCT),                             /* UINT16                  StructureLength;        */   \
+      HandlerGuid,                                                         /* GUID                    HandlerGuid;            */   \
+      HandlerPhysicalAddress,                                              /* UINT64                  HandlerPhysicalAddress  */   \
+    } \
+  }
+
+#endif // _PRMT_ACPI_TABLE_H_
