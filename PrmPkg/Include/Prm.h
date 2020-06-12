17b958a3dbcdac65dc4e87dd6a8d6d4f0cd3153c
Michael Kubacki <michael.kubacki@microsoft.com>
2020-06-12 14:27:18 -0700

    PrmPkg/Include/Prm.h: Add PRM_HANDLER prototype
    Adds a function prototype for PRM handlers.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- PrmPkg/Include/Prm.h
+++ PrmPkg/Include/Prm.h
@@ -11,6 +11,7 @@
 #define _PRM_H_
 
 #include <Uefi.h>
+#include <PrmContextBuffer.h>
 
 #define PRM_EXPORT_API                            __declspec(dllexport)
 
@@ -18,5 +19,21 @@
 
 #define PRM_STRING_(x)                            #x
 #define PRM_STRING(x)                             PRM_STRING_(x)
+
+/**
+  A Platform Runtime Mechanism (PRM) handler function.
+
+  @param[in]  ParameterBuffer             A pointer to a buffer with arbitrary data that is allocated and populated
+                                          by the PRM handler caller.
+  @param[in]  ContextBuffer               A pointer to a buffer with arbitrary data that is allocated in the firmware
+                                          boot environment.
+
+**/
+typedef
+EFI_STATUS
+(EFIAPI PRM_HANDLER) (
+  IN VOID                 *ParameterBuffer  OPTIONAL,
+  IN PRM_CONTEXT_BUFFER   *ContextBuffer  OPTIONAL
+  );
 
 #endif
