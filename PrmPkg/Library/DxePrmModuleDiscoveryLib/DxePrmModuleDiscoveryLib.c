1c01eb4fe6bbd6c10b7d95a02bd4e1cb12ea4d80
Michael Kubacki <michael.kubacki@microsoft.com>
2020-06-17 12:09:54 -0700

    PrmPkg/DxePrmModuleDiscoveryLib: Add initial host-based unit tests
    Adds host-based unit tests for DxePrmModuleDiscoveryLib. This is
    an initial set of support, more tests should be added in the
    future.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- PrmPkg/Library/DxePrmModuleDiscoveryLib/DxePrmModuleDiscoveryLib.c
+++ PrmPkg/Library/DxePrmModuleDiscoveryLib/DxePrmModuleDiscoveryLib.c
@@ -84,7 +84,6 @@ GetNextPrmModuleEntry (
                                           otherwise, NULL is returned.
 
 **/
-STATIC
 PRM_MODULE_IMAGE_CONTEXT_LIST_ENTRY *
 CreateNewPrmModuleImageContextListEntry (
   VOID
