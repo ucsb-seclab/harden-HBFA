tianocore/edk2

MdePkg: MmCommunication: Add new Host Software class Error Code to MdePkg
REF: https://bugzilla.tianocore.org/show_bug.cgi?id=3794

This change introduces a new error code definitions under Host Software
class.

The new error code definition will cover system reboot events under the
conditions of fragmented memory map from one boot to another.

These error codes could provide helpful datapoints to OEMs to investigate
and prevent system failures in general.

Cc: Michael D Kinney <michael.d.kinney@intel.com>
Cc: Liming Gao <gaoliming@byosoft.com.cn>
Cc: Zhiguang Liu <zhiguang.liu@intel.com>

Signed-off-by: Kun Qin <kuqin12@gmail.com>

MdePkg/Include/Pi/PiStatusCode.h
@@ -985,6 +985,7 @@ typedef struct {
 #define EFI_SW_EC_EVENT_LOG_FULL          0x00000011
 #define EFI_SW_EC_WRITE_PROTECTED         0x00000012
 #define EFI_SW_EC_FV_CORRUPTED            0x00000013
+#define EFI_SW_EC_FRAGMENTED_MEMORY_MAP   0x00000014
 ///@}
 
 //

ucsb-seclab/harden-HBFA

MdePkg/Include/Pi/PiStatusCode.h
@@ -985,6 +985,7 @@ typedef struct {
 #define EFI_SW_EC_EVENT_LOG_FULL          0x00000011
 #define EFI_SW_EC_WRITE_PROTECTED         0x00000012
 #define EFI_SW_EC_FV_CORRUPTED            0x00000013
+#define BZ3794_EFI_SW_EC_FRAGMENTED_MEMORY_MAP   0x00000014
 ///@}
 
 //