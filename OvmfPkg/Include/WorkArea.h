4ac52d8219441e016deb858386204c5914fc2def
Min M Xu <min.m.xu@intel.com>
2022-06-01 15:28:43 +0800

    OvmfPkg: Add MaxAcceptedMemoryAddress in TDX work area
    RFC: https://bugzilla.tianocore.org/show_bug.cgi?id=3937
    
    In lazy-accept the MaxAcceptedMemoryAddress is needed in
    TransferTdxHobList and ContructFwHobList(at PeilessStartupLib). But in
    SEC phase we cannot use a global variable to pass this value. So we add
    a new field (MaxAcceptedMemoryAddress) in Tdx work area.
    
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: James Bottomley <jejb@linux.ibm.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- OvmfPkg/Include/WorkArea.h
+++ OvmfPkg/Include/WorkArea.h
@@ -72,6 +72,7 @@ typedef struct _SEC_TDX_WORK_AREA {
   UINT32    Gpaw;
   UINT64    HobList;
   UINT64    PageTablePool;
+  UINT64    MaxAcceptedMemoryAddress;
 } SEC_TDX_WORK_AREA;
 
 typedef struct _TDX_WORK_AREA {
