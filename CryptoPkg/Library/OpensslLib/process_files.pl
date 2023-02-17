ea6db58f0908931064bfe214490129b2523d2060
Yi Li <yi1.li@intel.com>
2023-02-17 13:19:45 +0800

    CryptoPkg: process_files.pl: move all generated files to OpensslGen
    Signed-off-by: Yi Li <yi1.li@intel.com>

--- CryptoPkg/Library/OpensslLib/process_files.pl
+++ CryptoPkg/Library/OpensslLib/process_files.pl
@@ -547,7 +547,7 @@ print "Done!";
 #
 for my $file (map { s/\.in//; $_ } glob($OPENSSL_PATH . "/include/*/*.h.in")) {
     my $dest = $file;
-    $dest =~ s|.*/include/|../Include/|;
+    $dest =~ s|.*/include/|./OpensslGen/|;
     print "\n--> Duplicating $file into $dest ... ";
     system("perl -pe 's/\\n/\\r\\n/' < $file > $dest") == 0
         or die "Cannot copy $file !";
@@ -558,7 +558,16 @@ push @hdrs, glob($OPENSSL_PATH . "/providers/common/include/prov/[a-z]*.h");
 push @hdrs, glob($OPENSSL_PATH . "/providers/implementations/include/prov/[a-z]*.h");
 for my $file (@hdrs) {
     my $dest = $file;
-    $dest =~ s|.*/include/|../Include/|;
+    $dest =~ s|.*/include/|./OpensslGen/|;
+    print "\n--> Duplicating $file into $dest ... ";
+    system("perl -pe 's/\\n/\\r\\n/' < $file > $dest") == 0
+        or die "Cannot copy $file !";
+}
+
+for my $file (map { s/\.in//; $_ } glob($OPENSSL_PATH . "/providers/common/der/*.c.in")) {
+    next unless -f $file;
+    my $dest = $file;
+    $dest =~ s|.*/|./OpensslGen/|;
     print "\n--> Duplicating $file into $dest ... ";
     system("perl -pe 's/\\n/\\r\\n/' < $file > $dest") == 0
         or die "Cannot copy $file !";
