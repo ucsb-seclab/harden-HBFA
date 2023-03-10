6a4e5cecd35953d81b17d81c6f1f19e740ee77f8
Wenxing Hou <wenxing.hou@intel.com>
2023-03-10 17:59:17 +0800

    Add SysCall for BaseCryptLibMbedTls
    Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>

---
+++ CryptoPkg/Library/BaseCryptLibMbedTls/SysCall/CrtWrapper.c
@@ -0,0 +1,52 @@
+/** @file
+  C Run-Time Libraries (CRT) Wrapper Implementation for OpenSSL-based
+  Cryptographic Library.
+
+Copyright (c) 2009 - 2017, Intel Corporation. All rights reserved.<BR>
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <stdio.h>
+
+int mbedtls_printf (char const *fmt, ...)
+{
+  ASSERT(FALSE);
+  return 0;
+}
+
+int mbedtls_snprintf(char *str, size_t size, const char *format, ...)
+{
+  ASSERT(FALSE);
+  return 0;
+}
+
+int mbedtls_vsnprintf(char *str, size_t size, const char *format, ...)
+{
+  ASSERT(FALSE);
+  return 0;
+}
+
+void mbedtls_platform_zeroize( void *buf, unsigned int len )
+{
+    ZeroMem (buf, len);
+}
+
+int rand ()
+{
+  // TBD
+  return 1;
+}
+
+char *
+strchr (
+  const char  *str,
+  int         ch
+  )
+{
+  return ScanMem8 (str, AsciiStrSize (str), (char)ch);
+}
